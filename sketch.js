4e3f38e1d0a4d77524ac53b07c2bb7ab3094fbe2
penach0 <josemanuel.penacho@gmail.com>
2022-08-18 20:41:10 +0100

    Create 16x16 grid

---
+++ sketch.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container');
+let lines = [];
+let squares = [];
+
+for(let i = 0; i < 16; i++){
+    lines[i] = document.createElement('div');
+    lines[i].classList.add('line');
+    container.appendChild(lines[i]);
+    for(let j = 0; j < 16; j++){
+        squares[j] = document.createElement('div');
+        squares[j].classList.add('square');
+        lines[i].appendChild(squares[j]);
+        }
+    
+    }
