cc6d6342f14c391ba151fed4e449e0bd2771bcf7
penach0 <josemanuel.penacho@gmail.com>
2022-08-19 21:13:56 +0100

    Project complete
    -Add createGrid(size) function
    -Add clearGrid() function
    -Add askSize() function
    -Add restrictions to input

--- sketch.js
+++ sketch.js
@@ -1,18 +1,46 @@
 const container = document.getElementById('container');
 let lines = [];
 let squares = [];
+let initialSize = 16;
 
-for(let i = 0; i < 16; i++){
-    lines[i] = document.createElement('div');
-    lines[i].classList.add('line');
-    container.appendChild(lines[i]);
-    for(let j = 0; j < 16; j++){
-        squares[j] = document.createElement('div');
-        squares[j].classList.add('square');
-        lines[i].appendChild(squares[j]);
+function createGrid(gridSize){
+    clearGrid();
+    if(gridSize != initialSize) gridSize = askSize();
+    for(let i = 0; i < gridSize; i++){
+        lines[i] = document.createElement('div');
+        lines[i].classList.add('line');
+        container.appendChild(lines[i]);
+        for(let j = 0; j < gridSize; j++){
+            squares[j] = document.createElement('div');
+            squares[j].classList.add('square');
+            lines[i].appendChild(squares[j]);
+            }
         }
-    }
+    squares = [...document.getElementsByClassName('square')];
+    squares.forEach((square) => square.addEventListener('mouseover', () => square.classList.add('colored')));
+    
+}
 
-squares = [...document.getElementsByClassName('square')];
+function clearGrid() {
+    container.replaceChildren();
+}
 
-squares.forEach((square) => square.addEventListener('mouseover', () => square.classList.add('colored')));
+function askSize(){
+    let size = 81;
+    while(size > 80 || isNaN(size) ) {
+        size = +prompt("Please enter the size (max: 80): ");
+        if(isNaN(size)) {
+            alert("Please enter a number!!");
+            continue;
+        }else return(size);
+    };
+    
+}
+
+createGrid(initialSize);
+
+
+
+const button = document.getElementById('button');
+button.addEventListener('click', createGrid);
+
